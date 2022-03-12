e707a9a275fa3f6abe95421d38e82e7b5baadc82
heni8668 <henoktilahun363@gmail.com>
2022-03-12 00:25:49 -0800

    commit for I sometimes suffer from insomnia task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+*main - Printthe alphabetic
+*
+*Return: Always (success)
+*/
 int main(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+char c;
+for(c='a'; c<='z'; c++)
+  putchar(c);
+putchar('\n');
+return (0);
 }
+
