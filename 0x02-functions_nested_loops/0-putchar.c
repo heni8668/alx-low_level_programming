fd11857c5012a9fed75fb725cf1702a1bb750491
heni8668 <henoktilahun363@gmail.com>
2022-03-15 11:36:58 -0700

    commit for putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,6 +9,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
-
