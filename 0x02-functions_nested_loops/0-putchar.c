0f271b5fa42a9613b94015bbcc7daff2d400fa56
heni8668 <henoktilahun363@gmail.com>
2022-03-16 00:22:54 -0700

    commit for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,15 @@
  * main -entry block
  * @voud : no argument
  * Return: 0
- * */
+ */
 int main(void)
 {
 	char henok[] = "_putchar";
 	int c;
-	for (c = 0; c < 8; c++){
-		_putchar(henok [c]);
-	}
+	for (c = 0; c < 8; c++)
+{
+		_putchar(henok[c]);
+}
 	_putchar('\n');
 	return 0;
 }
