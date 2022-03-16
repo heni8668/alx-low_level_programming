7e4221a9d2247686d0b89648ee156cdb8a15bbe4
heni8668 <henoktilahun363@gmail.com>
2022-03-16 01:02:35 -0700

    commit for 10 x alphabet task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * 2-print_alphabet_x10.c - print the alphabets in lower case 10 times.
- * 
- * Return: always -.
- */
+* 2-print_alphabet_x10.c - print the alphabets in lower case 10 times.
+* 
+* Return: always -.
+*/
  void print_alphabet_x10(void)
  {
  	int i;
  	char c;
+
  	for (i = 0; i < 10; i++)
  	{
  		for (c = 'a'; c <= 'z'; c++)
