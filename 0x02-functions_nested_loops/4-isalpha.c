76491190874229523e7df7908460374011cbda5b
heni8668 <henoktilahun363@gmail.com>
2022-03-16 01:27:56 -0700

    commit for isalpha task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
-/* _isalpha -checks for alphabetic character
-* @c: takes in a character
-* Return: 1 if letter, lowercase, uppercase 0 for otherwise
-*/
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ *@c: The int to print
+ * Return: Always 0.
+ */
 
 int _isalpha(int c)
+
 {
-if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 {
 return (1);
 }
+
 else
 {
 return (0);
 }
+
 }
