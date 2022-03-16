089cc16490c374b33e59ce794c4d8ff214f296e5
heni8668 <henoktilahun363@gmail.com>
2022-03-16 05:40:32 -0700

    commit for The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,55 +1,52 @@
 #include "main.h"
 
 /**
- * print_times_table - prints times table for numbers from 0-14
- * @n: An input integer value
- * Return: Nothing
- */
+* print_times_table - This is a absolute value
+*
+* @n: This is the entry
+*
+* Return: Always 0.
+*/
+
 void print_times_table(int n)
 {
-	int i, j;
-
-	if (n > 0 && n < 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
-				putformat(i * j);
-			_putchar('\n');
-		}
-	}
-}
-
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
+if (n >= 0 && n <= 15)
 {
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
+int b, c, d;
+for (c = 0; c <= n; c++)
+{
+for (b = 0; b <= n; b++)
+{
+d = b * c;
+
+if (b == 0)
+_putchar(d + '0');
+else if (d <= 9)
+{
+_putchar(32);
+_putchar(32);
+_putchar(d + '0');
 }
+else if (d >= 10 && d <= 99)
+{
+_putchar(32);
+_putchar(((d / 10) % 10) + '0');
+_putchar((d % 10) + '0');
+}
+else
+{
+_putchar((d / 100) + '0');
+_putchar(((d / 10) % 10) + '0');
+_putchar((d % 10) + '0');
+}
+if (b != n)
+{
+_putchar(',');
+_putchar(32);
+}
+}
+_putchar('\n');
+}
+}
+}
+
