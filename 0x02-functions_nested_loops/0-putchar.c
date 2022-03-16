cdbf7d63581e73d24f4e27f99293b56f50148901
heni8668 <henoktilahun363@gmail.com>
2022-03-16 00:14:00 -0700

    commit for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,16 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * main -entry block
+ * @voud : no argument
+ * Return: 0
+ * */
+int main(void)
 {
-return (write(1, &c, 1));
+	char henok[] = "_putchar";
+	int c;
+	for (c = 0; c < 8; c++){
+		_putchar(henok[c]);
+	}
+	_putchar('\n');
+	retirn 0;
 }
