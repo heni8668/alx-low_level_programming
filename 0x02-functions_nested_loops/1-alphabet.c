18c193407eed9166254d758bcf928be64bde64d0
heni8668 <henoktilahun363@gmail.com>
2022-03-16 00:53:19 -0700

    commit for  I sometimes suffer from insomnia. And when I cant fall asleep, I play what I call the alphabet game task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet in lower case
- * main - check the code.
- *
- *Return: Always 0. 
- */
+* print_alphabet - prints alphabet in lower case
+* main - check the code.
+*
+*Return: Always 0. 
+*/
 void print_alphabet(void)
 {
 	char c;
