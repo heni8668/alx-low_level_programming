4b9d24f62ae2797e0f146b76f44c9e4cf3a4fcd9
heni8668 <henoktilahun363@gmail.com>
2022-03-21 10:53:53 -0700

    commit for 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/** @swap_int - a function that swaps the values of two integers.
+/**
+*@swap_int - a function that swaps the values of two integers.
 *@a: an inout integer pointer
 *@b: an input integer pointer
 *Return: Nothing
