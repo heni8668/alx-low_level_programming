8c406aa4fe5b3a54dc1809854d6782e6eb89c8a5
heni8668 <henoktilahun363@gmail.com>
2022-03-21 10:52:03 -0700

    commit for 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
-/** @swap_int - a function that swaps the values of two integers. 
-* @a: an inout integer pointer 
-* @b: an input integer pointer
-* Return: Nothing 
+/** @swap_int - a function that swaps the values of two integers.
+*@a: an inout integer pointer
+*@b: an input integer pointer
+*Return: Nothing
 */
-void swap_int(int *a, int *b) 
-{ 
-int aux; 
-aux = *a; 
-*a = *b; 
+void swap_int(int *a, int *b)
+{
+int aux;
+aux = *a;
+*a = *b;
 *b = aux;
 }
