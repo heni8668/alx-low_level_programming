71d4c0594bfa73f5c7f440a37acce85089342e16
heni8668 <henoktilahun363@gmail.com>
2022-03-21 10:32:45 -0700

    commit for 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    int a;
-    int b;
+int a;
+int b;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+a = 98;
+b = 42;
+printf("a=%d, b=%d\n", a, b);
+swap_int(&a, &b);
+printf("a=%d, b=%d\n", a, b);
+return (0);
 }
