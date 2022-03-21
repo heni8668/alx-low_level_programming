8f14a935877a8ea1bc1022e7afdb505ce5ba2752
heni8668 <henoktilahun363@gmail.com>
2022-03-21 10:40:57 -0700

    commit for 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,9 +2,9 @@
 
 /**
 * main - check the code for Holberton School students.
-*swap_int - swap the number
-*@a - number 
-*@b - number
+* swap_int - swap the number
+* @a - number 
+* @b - number
 * Return: Always 0.
 */
 void swap_int(int *a, int *b)
