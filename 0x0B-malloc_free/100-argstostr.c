dcef2679053fdb6d53c2c5d247117591838860cc
heni8668 <henoktilahun363@gmail.com>
2022-04-04 00:30:40 -0700

    commit for 100-argstostr.c task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,49 +1,52 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+int _strlen(char *ch);
 
 /**
-* *argstostr - concatenates all the arguments of the program.
-*
-* @ac: number of arguments.
-*
-* @av: array of arguments
-*
-* Return: Pointer to the new string (Success), NULL (Error)
+* argstostr - concatenate arguments.
+* @ac: arg counter.
+* @av: arg vector.
+* Return: pointer p.
 */
-
 char *argstostr(int ac, char **av)
 {
-int i, j, k, len;
-char *str;
-
+int i, a = 0, k = 0;
+int j;
+char *p;
 if (ac == 0 || av == NULL)
-return (NULL);
-
+return (0);
 for (i = 0; i < ac; i++)
 {
-for (j = 0; av[i][j] != '\0'; j++)
-len++;
-len++;
+a += _strlen(av[i]);
 }
-
-str = malloc(sizeof(char) * (len + 1));
-
-if (str == NULL)
-return (NULL);
-
-k = 0;
-
+p = malloc(a + 1 + ac);
+if (p == NULL)
+return (0);
 for (i = 0; i < ac; i++)
 {
-for (j = 0; av[i][j] != '\0'; j++)
+for (j = 0; j < (_strlen(av[i])); j++)
 {
-str[k] = av[i][j];
+p[k] = av[i][j];
 k++;
 }
-str[k] = '\n';
+p[k] = '\n';
 k++;
+}
+p[k] = '\0';
+return (p);
 }
 
-return (str);
+/**
+* _strlen - counts number of chars.
+* @ch: string.
+* Return: pointer i.
+*/
+
+int _strlen(char *ch)
+{
+int i = 0;
+while (ch[i])
+i++;
+return (i);
 }
