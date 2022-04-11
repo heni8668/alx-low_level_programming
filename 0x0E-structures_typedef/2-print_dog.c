645feac5a449943fdac2cc176fe39622e9ed02e2
heni8668 <henoktilahun363@gmail.com>
2022-04-11 04:18:38 -0700

    commit for 2-print_dog.c task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - ID generator for Django
+ * @d: dog data structure
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name != NULL)
+		printf("Name: %s\n", (*d).name);
+	else
+		printf("Name: (nil)\n");
+	printf("Age: %f\n", (*d).age);
+	if ((*d).name != NULL)
+		printf("Owner: %s\n", (*d).owner);
+	else
+		printf("Owner: (nil)\n");
+}
