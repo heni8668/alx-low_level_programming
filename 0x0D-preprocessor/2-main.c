69525c5362da33f0fadcc97e25c49abe674aab58
heni8668 <henoktilahun363@gmail.com>
2022-04-11 03:22:41 -0700

    commit for 2-main.c task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include < stdlib.h>
+/**
+*main - prints the name of a file it was compile from
+*
+*Return: Always 0 on success
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
