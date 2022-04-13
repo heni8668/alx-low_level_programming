b5d777a4471e16ec1f9f0a92194346ca5fc8c9b3
heni8668 <henoktilahun363@gmail.com>
2022-04-13 09:57:57 -0700

    commit for 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,34 +2,43 @@
 #include <stdlib.h>
 
 /**
- * main - prints opcode of own main function
- * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
+ * main - prints the opcodes of its own main function
+ *
+ * @argv: the argument that enters to the program
+ * @argc: counts the number of arguments
+ *
+ * Return: the opcode hex number
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int bytes, i;
-unsigned char *func_ptr;
+int runner = 0, bytes;
+/* the tasks tell us each opcode is two char long */
+/* if the argv is less or more then Error */
 
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
+/* the argument in position one (the number the program receives) */
+/* will be equal to bytes */
 bytes = atoi(argv[1]);
+/* condition if the number of bytes y negative (less than cero) */
+/* exit status 2 and print Error */
 if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
 }
-func_ptr = (unsigned char *)main;
-i = 0;
-if (bytes > 0)
+/* we use runner to go through the array we receive in the argv */
+while (runner < bytes)
 {
-while (i < (bytes - 1))
-printf("%02hhx ", func_ptr[i++]);
-printf("%hhx\n", func_ptr[i]);
+/* is pre define how to print an opcodes */
+printf("%02hhx", *((char *)(main + runner)));
+if (bytes > runner + 1)
+printf(" ");
+runner++;
 }
+printf("\n");
 return (0);
 }
