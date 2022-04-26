d02461f0b042c717d63346defd441600d44f95c0
heni8668 <henoktilahun363@gmail.com>
2022-04-26 09:08:15 -0700

    commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
- *
- * @head: list of type listint_t to print
- *
- * Return: number of nodes in the list
- */
+* print_listint_safe - prints a linked list, safely
+*
+* @head: list of type listint_t to print
+*
+* Return: number of nodes in the list
+*/
 
 size_t print_listint_safe(const listint_t *head)
 {
